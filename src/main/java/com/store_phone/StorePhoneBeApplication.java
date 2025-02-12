b13b818790975b4d400e7bba007e1fb7f05c2954
tuankul <thaithanhtuan43@gmail.com>
2025-02-12 19:49:50 +0700

    init

---
+++ src/main/java/com/store_phone/StorePhoneBeApplication.java
@@ -0,0 +1,13 @@
+package com.store_phone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorePhoneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorePhoneBeApplication.class, args);
+	}
+
+}
